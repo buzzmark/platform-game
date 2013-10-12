d567ac9d0a76d7d0f9d3a5e6d63e52863a776d6a
Matthew Stromberg <mcmcmatthew@gmail.com>
2013-10-11 22:28:24 -0500

    Add jumping/falling animation
    Fixes #4

--- platform-game/src/buzzmark/platformer/entity/Player.java
+++ platform-game/src/buzzmark/platformer/entity/Player.java
@@ -11,10 +11,10 @@ import buzzmark.platformer.utils.Keyboard;
 public class Player extends Movable {
 	
 	private Keyboard keyboard;
-	// private Sprite sprite;
-	private Animation animation;
-	private Animation runningLeft, runningRight;
-	private Animation idleLeft, idleRight;
+	private Animation currentAnimation;
+	private final Animation runLeftAnimation, runRightAnimation;
+	private final Animation idleLeftAnimation, idleRightAnimation;
+	private final Animation fallLeftAnimation, fallRightAnimation;
 	private double movespeed = 1.5;
 	private double jumpspeed = -4.0;
 	
@@ -33,25 +33,31 @@ public class Player extends Movable {
 		}
 		Sprite[] playerRunningLeft = new Sprite[6];
 		for (int i = 5; i >= 0; i--) {
-			playerRunningLeft[5-i] = new Sprite(16, 32, i * 16, 2 * 32, SpriteSheet.playerSheet);
+			playerRunningLeft[5 - i] = new Sprite(16, 32, i * 16, 2 * 32, SpriteSheet.playerSheet);
 		}
 		
 		int[] steps = new int[6];
 		Arrays.fill(steps, 7);
-		runningRight = new Animation(playerRunningRight, steps);
-		runningLeft = new Animation(playerRunningLeft, steps);
+		runRightAnimation = new Animation(playerRunningRight, steps);
+		runLeftAnimation = new Animation(playerRunningLeft, steps);
 		
 		Sprite[] playerIdleRight = new Sprite[2];
-		playerIdleRight[0] = new Sprite(16, 32, 1 * 16, 0, SpriteSheet.playerSheet);
-		playerIdleRight[1] = new Sprite(16, 32, 0 * 16, 0, SpriteSheet.playerSheet);
-		idleRight = new Animation(playerIdleRight, new int[] { 40, 40 });
+		playerIdleRight[0] = new Sprite(16, 32, 0 * 16, 0, SpriteSheet.playerSheet);
+		playerIdleRight[1] = new Sprite(16, 32, 1 * 16, 0, SpriteSheet.playerSheet);
+		idleRightAnimation = new Animation(playerIdleRight, new int[] { 40, 40 });
 		
 		Sprite[] playerIdleLeft = new Sprite[2];
-		playerIdleLeft[0] = new Sprite(16, 32, 4 * 16, 0, SpriteSheet.playerSheet);
-		playerIdleLeft[1] = new Sprite(16, 32, 5 * 16, 0, SpriteSheet.playerSheet);
-		idleLeft = new Animation(playerIdleLeft, new int[] { 40, 40 });
+		playerIdleLeft[0] = new Sprite(16, 32, 5 * 16, 0, SpriteSheet.playerSheet);
+		playerIdleLeft[1] = new Sprite(16, 32, 4 * 16, 0, SpriteSheet.playerSheet);
+		idleLeftAnimation = new Animation(playerIdleLeft, new int[] { 40, 40 });
 		
-		animation = idleRight;
+		Sprite[] playerFallRight = { new Sprite(16, 32, 2 * 16, 0, SpriteSheet.playerSheet) };
+		fallRightAnimation = new Animation(playerFallRight, new int[] { 40 });
+		
+		Sprite[] playerFallLeft = { new Sprite(16, 32, 3 * 16, 0, SpriteSheet.playerSheet) };
+		fallLeftAnimation = new Animation(playerFallLeft, new int[] { 40 });
+		
+		currentAnimation = idleRightAnimation;
 	}
 	
 	public void jump() {
@@ -63,27 +69,36 @@ public class Player extends Movable {
 	public void update() {
 		if (keyboard.isKeyPressed(KeyEvent.VK_A) || keyboard.isKeyPressed(KeyEvent.VK_LEFT)) {
 			velocity.setX(-movespeed);
-			if (animation != runningLeft) {
-				animation.reset();
-				animation = runningLeft;
+			if (!isInAir && currentAnimation != runLeftAnimation) {
+				currentAnimation.reset();
+				currentAnimation = runLeftAnimation;
 			}
 		} else if (keyboard.isKeyPressed(KeyEvent.VK_D) || keyboard.isKeyPressed(KeyEvent.VK_RIGHT)) {
 			velocity.setX(movespeed);
-			if (animation != runningRight) {
-				animation.reset();
-				animation = runningRight;
+			if (!isInAir && currentAnimation != runRightAnimation) {
+				currentAnimation.reset();
+				currentAnimation = runRightAnimation;
 			}
 		} else {
 			velocity.setX(0.0);
-			if (direction == LEFT && animation != idleLeft) {
-				animation.reset();
-				animation = idleLeft;
-			} else if (direction == RIGHT && animation != idleRight) {
-				animation.reset();
-				animation = idleRight;
+			if (!isInAir && direction == LEFT && currentAnimation != idleLeftAnimation) {
+				currentAnimation.reset();
+				currentAnimation = idleLeftAnimation;
+			} else if (!isInAir && direction == RIGHT && currentAnimation != idleRightAnimation) {
+				currentAnimation.reset();
+				currentAnimation = idleRightAnimation;
 			}
 		}
-		animation.update();
+		if (isInAir) {
+			if (direction == LEFT && currentAnimation != fallLeftAnimation) {
+				currentAnimation.reset();
+				currentAnimation = fallLeftAnimation;
+			} else if (direction == RIGHT && currentAnimation != fallRightAnimation) {
+				currentAnimation.reset();
+				currentAnimation = fallRightAnimation;
+			}
+		}
+		currentAnimation.update();
 		
 		if (keyboard.isKeyPressed(KeyEvent.VK_SPACE) && canJump)
 			jump();
@@ -93,8 +108,7 @@ public class Player extends Movable {
 	
 	@Override
 	public void render(Screen screen) {
-		// screen.renderSprite(position, sprite);
-		animation.render(position, screen);
+		currentAnimation.render(position, screen);
 		// super.render(screen); // TODO remove later, renders collision box for testing
 	}
 	
